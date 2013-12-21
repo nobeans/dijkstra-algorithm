6e2c4cd33a3fafc2ac02a72a8a55237c5345560b
Yasuharu Nakano <ynak@jggug.org>
2013-12-21 17:56:00 +0900

    その時点の未評価＆最小コストのノードから順番に評価していくのがポイントのようだ
    http://www.ss.cs.meiji.ac.jp/CCP024.html
    http://www.me.sophia.ac.jp/or/lab/ishizuka/OC/spath_00.html

--- dijkstra.groovy
+++ dijkstra.groovy
@@ -1,10 +1,14 @@
+@Grab(group='ch.qos.logback', module='logback-classic', version='1.0.13')
+import groovy.util.logging.Slf4j
+
 // Define Class
+@Slf4j
 class Node {
     def id
     def edges = []
     int cost = Integer.MAX_VALUE // 最小コスト
-    Node from      // 最小コスト時はどのノードからきたときか
-    boolean done = false
+    Node from                    // 最小コストはどのノードからきたときか
+    boolean done = false         // 評価済みフラグ
 
     String toString() {
         "${done ? '*' : ' '}node#${id}${edges}:${fromPath}"
@@ -14,9 +18,10 @@ class Node {
         (from ? from.fromPath : '') + "/node#${id}(${cost})"
     }
 
-    void calculateAdjacentNodeCost() {
+    // このノードを評価して隣接ノードのコストに反映する。
+    void evalNode() {
         if (done) return
-        println "calculateAdjacentNodeCost(): begin: ${this}"
+        log.debug "evalNode(): begin: ${this}"
         edges.each { edge ->
             // 各接続先ノードのコストがこのノード経由の方がコストが低いか？
             def toNode = edge.to
@@ -25,27 +30,53 @@ class Node {
                 // 最小コストを更新する。
                 toNode.cost = costToNodeFromThisNode
                 toNode.from = this
-                println "calculateAdjacentNodeCost(): updated edege node: ${toNode}"
+                log.debug "evalNode(): updated edges node: ${toNode}"
+            } else {
+                log.debug "evalNode(): ignored edges node: ${toNode}"
             }
         }
         done = true
+        log.debug "evalNode(): done: ${this}"
+    }
+}
 
-        // 隣接ノードがまだコスト計算していなければ、再帰的にコスト計算する。
-        // TODO この順序が間違っていると答えが間違ってしまう。スタートノードから隣接ノード
-        edges.each { edge ->
-            def toNode = edge.to
-            if (!toNode.done) {
-                toNode.calculateAdjacentNodeCost()
-            }
+@Slf4j
+class NodeList {
+    def nodes = [:]
+
+    NodeList(Iterable range) {
+        range.each {
+            nodes[it] = new Node(id: it)
         }
-        println "calculateAdjacentNodeCost(): done: ${this}"
+    }
+
+    def getAt(key) {
+        nodes[key]
+    }
+
+    String toString() {
+        nodes.collect { it }.join(System.getProperty("line.separator"))
+    }
+
+    void calculateCostFrom(startNode) {
+        // スタートノードのコストは0
+        startNode.cost = 0
+        while (true) {
+            // その時点の未評価＆最小コストのノードから順番に評価していく
+            def node = minCostNode
+            if (!node) return
+            node.evalNode()
+        }
+    }
+
+    private getMinCostNode() {
+        nodes.findAll { id, node -> !node.done }.sort { it.value.cost }.find()?.value
     }
 }
 
 class Edge {
-    def id
-    Node to         // 接続先ノード
-    int cost = 0    // 接続先ノードへの必要コスト
+    Node to      // 接続先ノード
+    int cost = 0 // 接続先ノードへの必要コスト
 
     String toString() {
         "(${cost})->node#${to?.id}"
@@ -53,14 +84,7 @@ class Edge {
 }
 
 // Setup sample data
-def nodes = [
-    1: new Node(id: 1), // start node
-    2: new Node(id: 2),
-    3: new Node(id: 3),
-    4: new Node(id: 4),
-    5: new Node(id: 5),
-    6: new Node(id: 6), // goal node
-]
+def nodes = new NodeList(1..6)
 
 nodes[1].edges = [
     new Edge(cost: 5, to: nodes[2]),
@@ -97,7 +121,7 @@ nodes[6].edges = [
 
 def dumpNodes = { label = '' ->
     println "${label}" + "-"*10
-    nodes.each { println it }
+    println nodes
 }
 
 // スタートとゴールの指定
@@ -106,17 +130,11 @@ def goalNode = nodes[6]
 
 dumpNodes("Initial")
 
-// スタートノードのコストは0
-startNode.cost = 0
-
-dumpNodes()
-
-// スタートノードから再帰的にコスト計算する
-startNode.calculateAdjacentNodeCost()
+nodes.calculateCostFrom(startNode)
 
 dumpNodes("Done")
 
 // スタートからゴールまでの道を表示する
-
-
+println "Answer:" + "-"*50
+println goalNode.fromPath
 
